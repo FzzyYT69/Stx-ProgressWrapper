391fe7f743ec0bae4a71aecd9697825e4fcdd7e4
FzzY#2284 <99145322+FzzyYT69@users.noreply.github.com>
2024-01-18 16:16:13 +0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,8 @@
 -- Resource Metadata
 fx_version 'cerulean'
-games { 'gta5' }
+games { 'gta5', 'rdr3' }
+
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 author '_fzzy'
 description 'ProgressBar Selector v2'
